d96c6b0a388c88a7f2ff6493e0785fb3317578a1
ImaLuckyMan <Ima_Lucky_Man@yahoo.com>
2020-02-20 07:44:44 -0800

    Add project files.

---
+++ Palindrome.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Palindrome
+    {
+        static void Main(string[] args)
+        {
+            IsPalindrome("racecar");
+            IsPalindrome("cat");
+            IsPalindrome("mom");
+            IsPalindrome("tacocat");
+        }
+
+        public static void IsPalindrome(string input)
+        {  
+            bool isPal = true;
+            
+            if ( input == "" || input == null)
+            {
+                isPal = false;
+            }
+            
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] != input[input.Length - 1 - i])
+                {
+                    isPal = false;
+                }
+                
+            }
+            Console.WriteLine(isPal); 
+        }
+
+    }
+}
